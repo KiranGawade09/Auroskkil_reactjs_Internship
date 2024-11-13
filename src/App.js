12b11f4fca198beb5bc5318a35aa16ad42127215
KiranGawade09 <91245102+KiranGawade09@users.noreply.github.com>
2024-11-13 22:39:02 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ import './App.css';
 
 
 function App(){
+
+  var object ={
+    background:"red"
+  }
+  var Jack ="kiran";
+  var Array =[1,2,4,5,6];
+
   return(
     <>
-    <h1>hello React app here</h1>
+    <h1>Hi my name is {Jack} </h1>
+    <h1 style={{background:"grey"}}>hello React app here</h1>
     <p id="txt1">kiran gawade here</p>
+    <h1>Array values are {Array}</h1>
+    {
+      Array.map((x)=>(
+           <h1>{x}</h1>
+      ))
+   
+    }
+    <p style={{backgroundColor:object.background}}>This is scc from object values</p>
     <img src={image1} height={'250px'} width={'auto'}/>
     </>
   )
