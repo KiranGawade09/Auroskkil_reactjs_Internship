ba5ccb7dc849fb67677d15ddc6317cc7c8d4e3e7
KiranGawade09 <91245102+KiranGawade09@users.noreply.github.com>
2024-11-13 22:14:41 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import image1 from '../src/image/img1.png';
+import './App.css';
 
 
 function App(){
   return(
+    <>
     <h1>hello React app here</h1>
+    <p id="txt1">kiran gawade here</p>
+    <img src={image1} height={'250px'} width={'auto'}/>
+    </>
   )
 }
 
